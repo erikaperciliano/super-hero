9f1d3b1338730c16b24efa3e79d0414243afdb8e
Erika Perciliano <erikaperciliano.developer@gmail.com>
2021-03-14 21:38:42 -0300

    add componente da busca

---
+++ src/components/ui/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from '../../img/logo.jpg';
+
+
+const Header = (props) => {
+    const {handleChange, searchText} = props;
+    
+    return (
+        <>
+            <header className="center">
+                <img src={logo} alt="logo"/>
+            </header>
+            <section className="search">
+                <input
+                    id="search-bar"
+                    type="search"
+                    placeholder="Search superheroes here..."
+                    onChange={handleChange}
+                    value={searchText}
+                    className="form-control"
+                    autoFocus
+                /> 
+            </section>
+        </>
+    )
+}
+
+export default Header;
